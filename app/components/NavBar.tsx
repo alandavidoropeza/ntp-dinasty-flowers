51df210d2ba816158aad9643eaa94d2a2854ce51
Alan Oropeza <alandavidoropeza@gmail.com>
2025-09-19 16:12:57 -0400

    hero-v1-1

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar = () => {
 const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className='fixed top-0 left-0 right-0 z-50 border-b border-border'>
+    <nav className='fixed top-0 left-0 right-0 z-50 bg-background/95 backdrop-blur-sm border-b border-border'>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
             {/*Desktop Menu */}
