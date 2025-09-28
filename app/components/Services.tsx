175ff4804ea91a75e76a61215a747a02e4b42791
Alan Oropeza <alandavidoropeza@gmail.com>
2025-09-27 20:26:30 -0400

    add-services-section

---
+++ app/components/Services.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <section id='services' className='py-20 bg-gradient-nature'>
+
+    </section>
+  )
+}
+
+export default Services
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import AboutUs from "./components/AboutUs";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <AboutUs />
+      <Services />
     </>
   );
 }
